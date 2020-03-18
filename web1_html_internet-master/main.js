f744bf329848f48e048234c0250a1dff9a14b669
jaelin <dncgr8@gmail.com>
2020-03-19 01:15:02 +0900

    main js updated

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,48 +1,67 @@
 var http = require('http');
-const fs = require('fs');
-const url = require('url');
+var fs = require('fs');
+var url = require('url');
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
-    console.log(_url); //  /2.html?id=HTML
-    var queryData = url.parse(_url,true).query;
-    console.log(queryData); // { id: 'CSS' } <- Query String 정보가 넘어옴
-    console.log(queryData.id); // CSS
-    var title = queryData.id;
-    if(_url == '/?id=Web'){
-      title = 'Welcome';
-      queryData.id = "WEB";
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}` , 'utf8' , (err, description) => {
-      if (err) throw err;
-      var template = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/?id=Web">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  
-      `;
-      response.end(template);
-    })
-
- 
 });
 app.listen(3000);
