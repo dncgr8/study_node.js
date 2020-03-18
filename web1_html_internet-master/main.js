87a5ec28ea38fdb0bbf106cf8ca5e81a8929a7aa
jaelin <dncgr8@gmail.com>
2020-03-18 23:17:28 +0900

    본문 change

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const fs = require('fs');
+const url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     console.log(_url); //  /2.html?id=HTML
@@ -8,8 +8,9 @@ var app = http.createServer(function(request,response){
     console.log(queryData); // { id: 'CSS' } <- Query String 정보가 넘어옴
     console.log(queryData.id); // CSS
     var title = queryData.id;
-    if(_url == '/'){
+    if(_url == '/?id=Web'){
       title = 'Welcome';
+      queryData.id = "WEB";
     }
     if(_url == '/favicon.ico'){
         response.writeHead(404);
@@ -17,30 +18,31 @@ var app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-  </p>
-</body>
-</html>
+    fs.readFile(`data/${queryData.id}` , 'utf8' , (err, description) => {
+      if (err) throw err;
+      var template = `
+      <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/?id=Web">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+  
+      `;
+      response.end(template);
+    })
 
-    `;
-    response.end(template);
  
 });
 app.listen(3000);
