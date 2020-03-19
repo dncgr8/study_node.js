0becc6926801f865ea2165940157ef06dedfbb4b
jaelin <dncgr8@gmail.com>
2020-03-19 18:08:51 +0900

    sync , ansync

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -8,9 +8,16 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        
           var title = 'Welcome';
           var description = 'Hello, Node.js';
+          fs.readdir('./data', function(error, filelist){
+            var list = '<ul>';
+            for(var i =0 ; i <filelist.length ;i++){
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            }
+              list = list +'</ul>';
+         
           var template = `
           <!doctype html>
           <html>
@@ -20,11 +27,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+                ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -32,10 +35,16 @@ var app = http.createServer(function(request,response){
           `;
           response.writeHead(200);
           response.end(template);
-        });
+        })
       } else {
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
+          fs.readdir('./data', function(error, filelist){
+            var list = '<ul>';
+            for(var i =0 ; i <filelist.length ;i++){
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            }
+              list = list +'</ul>';
           var template = `
           <!doctype html>
           <html>
@@ -45,11 +54,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+                ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -57,6 +62,7 @@ var app = http.createServer(function(request,response){
           `;
           response.writeHead(200);
           response.end(template);
+          })
         });
       }
     } else {
