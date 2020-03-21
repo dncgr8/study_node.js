69e8949e79173096e4f630a1bdad595e2c36bca2
jaelin <dncgr8@gmail.com>
2020-03-21 18:27:12 +0900

    delete-process

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -59,7 +59,13 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list, 
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a>  <a href="/update?id=${title}">update</a>`);
+              `<a href="/create">create</a>  
+              <a href="/update?id=${title}">update</a>
+              <form action="/delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+              </form>
+              `);
             response.writeHead(200);
             response.end(template);
           });
@@ -139,6 +145,22 @@ var app = http.createServer(function(request,response){
           response.end();
           }) 
         });
+
+      });
+    }else if(pathname ==='/delete_process'){
+      var body = ``;
+      request.on('data', function(data){
+         body = body + data;
+      });
+
+      request.on('end', function(){
+        var post = qs.parse(body);
+        console.log(post);
+        var id = post.id;
+       fs.unlink(`data/${id}`, function(error){
+        response.writeHead(302, {Location:`/`});
+        response.end();
+       }) 
 
       });
     }
