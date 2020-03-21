df248db4184ceed0e892a8962345fef640127689
jaelin <dncgr8@gmail.com>
2020-03-21 18:05:31 +0900

    update-process-save

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -109,7 +109,7 @@ var app = http.createServer(function(request,response){
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
               <p>
-              <textarea name="decription" placeholder="description">${description}</textarea>
+              <textarea name="description" placeholder="description">${description}</textarea>
               </p>
               <p>
                 <input type="submit">
@@ -120,6 +120,26 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+      });
+    }else if(pathname ==='/update_process'){
+      var body = ``;
+      request.on('data', function(data){
+         body = body + data;
+      });
+
+      request.on('end', function(){
+        var post = qs.parse(body);
+        console.log(post);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}` , description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+          }) 
+        });
+
       });
     }
     else {
